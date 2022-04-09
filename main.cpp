e9b9f532c646dbb3ee6f5b0ce6391152cdbd164c
liangzilixueha <542876162@qq.com>
2022-04-09 22:14:11 +0800

    v.0.1.14.0409卡牌放置，链表更新
    卡牌放置完成，链表的加入了打印函数，使得更加方便显示

--- main.cpp
+++ main.cpp
@@ -65,6 +65,7 @@ void Draw()
         Q = Head->val;
         window.draw(Q.Sprite);
         Q.txtFollow();
+
         //下一个链表内容
         Head = Head->next;
     }
@@ -88,6 +89,8 @@ void Draw()
         c.setPosition(i * WIDTH / 50, HEIGHT * 2 / 3);
         window.draw(c);
     }
+    c.setPosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+    window.draw(c);
     window.display();
 }
 int main()
@@ -99,64 +102,32 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
             {
-                printf("Left is press\n");
-                Head = CardHand->next;
-                while (Head)
+                // printf("Left is press\n");
+                //最后一个head
+                printf("CardHand: ");
+                CardHand->print();
+                Head = CardHand;
+                while (Head->next)
                 {
-                    if (Head->val.isInclude())
-                        Head->val.changeHold();
-                    Head = Head->next;
+                   sss Head = Head->next;
                 }
-
-                if(鼠标上没有被抓手牌)
+                if (isChooseCard == 0) //鼠标上没有被抓手牌
                 {
-                    //获取鼠标的位置
-                    mousePoint=sf::Mouse::getPosition(window)//获取鼠标位置，未定义 解决：定义或者类似的东西
-                    //对比手牌
-                    //遍历手牌的位置
-                    //按照手牌的序号越大就越在上面
-                    //建议从后开始遍历第一个满足的就是目标手牌
-                    //将手牌状态变为被抓手牌
+                    while (Head)
+                    {
+                        if (Head->val.isInclude())
+                        {
+                            Head->val.Hold = 1;
+                            break;
+                        }
+                        Head = Head->prior;
+                    }
                 }
-                // while (Head)
-                // {
-                //     // //如果鼠标在其中
-                //     // if (Head->val.isInclude())
-                //     // {
-                //     //     if (isChooseCard)
-                //     //         isChooseCard = 0;
-                //     //     else
-                //     //         isChooseCard = 1;
-                //     //     if (isChooseCard)
-                //     //         Head->val.changeHold();
-                //     //     //如果卡牌被选中
-                //     //     if (Head->val.Hold)
-                //     //     {
-                //     //         //卡牌的Y坐标
-                //     //         double y = Head->val.Sprite.getPosition().y;
-                //     //         //如果卡牌位于敌方位置，回去
-                //     //         //如果卡牌位于自己的下方，回去
-                //     //         // else让卡牌从手牌移除，加入战斗卡牌列表
-                //     //         if (y < HEIGHT / 2 || y > HEIGHT * 2 / 3)
-                //     //         {
-                //     //             Head->val.moveFlag = 1;
-                //     //         }
-                //     //         else
-                //     //         {
-                //     //             //插入这个节点到战斗手牌中
-                //     //             CardinFight->Insert(Head->val);
-                //     //             //删除这个节点
-                //     //             Head->prior->next = Head->next;
-                //     //         }
-                //     //     }
-                //     //     //只要鼠标在卡牌的范围内，那每一次点击就必然会拿起一张卡牌或者放下一张卡牌
-                //     //     //如果卡牌被选中，那么就放下这张卡牌
-                //     //     //如果卡牌没有被选中，那么就拿起这张卡牌
-                //     Head = Head->next;
-                // }
             }
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right)
             {
@@ -172,29 +143,35 @@ int main()
                     Head = Head->next;
                 }
             }
-            if(event.type==sf::Event::MouseButtonReleased && event.mouseButton.button==sf::Mouse::Left)
+            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
             {
-                printf("Left is release\n");
+                // printf("Left is release\n");
                 //当鼠标抓着手牌的时候↓
                 Head = CardHand->next;
                 while (Head)
                 {
-                    mousePoint=sf::Mouse::getPosition(window)//获取鼠标位置，未定义 解决：定义或者类似的东西
-                    //要求此时鼠标位置和被抓卡牌在同一位置
-                    if(mousPoint.y>=对战区域的上半边界 && mousPoint.y<=对战区域的下半边界)
+                    Head->val.Hold = 0;
+                    // if (sf::Mouse::getPosition().y >= HEIGHT / 2 && sf::Mouse::getPosition().y <= HEIGHT * 2 / 3)
+                    if (Head->val.Sprite.getPosition().y >= HEIGHT / 2 && Head->val.Sprite.getPosition().y <= HEIGHT * 2 / 3)
                     {
-                        //下方是自己生成的，不知道对不对
-                        //要求将被抓的卡牌放到对战区域
-                        //插入这个节点到战斗手牌中的一个函数
-                        // if(Head->val.Hold)
-                        // {
-                        //     Head->val.moveFlag=1;
-                        //     CardinFight->Insert(Head->val);
-                        //     Head->prior->next=Head->next;
-                        // }
-                    }else{
-                        //被抓的卡牌放到手牌中
-
+                        CardinFight->Insert(Head->val);
+                        printf("CardinFight:");
+                        CardinFight->print();
+                        if (Head->next == NULL)
+                        {
+                            Head->prior->next = NULL;
+                        }
+                        else if (Head->next->next == NULL)
+                        {
+                            Head->val = Head->next->val;
+                            Head->next = Head->next->next;
+                        }
+                        else
+                        {
+                            Head->val = Head->next->val;
+                            Head->next = Head->next->next;
+                            Head->next->prior = Head;
+                        }
                     }
                     Head = Head->next;
                 }
