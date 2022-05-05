b42af807760486d412a80a102b2b7df4ab9a352a
Eanya-Tonic <74545593+Eanya-Tonic@users.noreply.github.com>
2022-05-05 19:19:05 +0800

    删除无用注释

--- main.cpp
+++ main.cpp
@@ -123,11 +123,8 @@ void Round()
         window.draw(YRound.Sprite);
     else if (IsYourRound && IsPressed)
         window.draw(YRound_Down.Sprite);
-    // 为修复bug被迫注释
-    
     else
         window.draw(ERound.Sprite);
-    
 }
 
 // 开始初始化
